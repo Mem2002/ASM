6d94899c38381873272ebf482616f7ca8441ae59
Mem2002 <anhnvngch200176@fpt.edu.vn>
2022-10-20 14:27:59 +0700

    first commit

--- lab9/routes/laptop.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  var name = "Laptop Router";
-  res.render("index", { data: name });
-});
-
-module.exports = router;
